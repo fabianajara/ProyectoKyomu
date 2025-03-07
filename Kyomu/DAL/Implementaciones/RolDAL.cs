ef8963cec95965adfd6de6565207ac7d2ae5db63
Ari8012 <aaraya50091@ufide.ac.cr>
2025-03-06 20:30:14 -0600

    Creación del Backend con la mayoría de entidades.

---
+++ Kyomu/DAL/Implementaciones/RolDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Interfaces;
+using Entities.Entities;
+
+namespace DAL.Implementations
+{
+    public class RolDAL : DALGenericoImpl<Rol>, IRolDAL
+    {
+        public KyomuContext _context;
+
+        public RolDAL(KyomuContext context) : base(context)
+        {
+            _context = context;
+
+        }
+
+        public List<Rol> GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
