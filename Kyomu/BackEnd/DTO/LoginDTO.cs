1de9fbb61b54b489915a25d90c616528ee97920e
Ari8012 <aaraya50091@ufide.ac.cr>
2025-04-08 15:45:59 -0600

    Seguridad Completa.

---
+++ Kyomu/BackEnd/DTO/LoginDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BackEnd.DTO
+{
+    public class LoginDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public TokenDTO? Token { get; set; }
+
+        public List<string>? Roles { get; set; }
+    }
+}
