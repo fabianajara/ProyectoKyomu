73c1ab8ac3181c277523322488feef275624151f
Fabiana Jara <fabianajara.u@gmail.com>
2025-04-13 16:28:11 -0600

    Cambios Home
    Ahora en el home se traen los platillos de la base de datos. Se van a mostrar en un tab distinto dependiendo de su categoría.

--- Kyomu/FronEnd/Controllers/HomeController.cs
+++ Kyomu/FronEnd/Controllers/HomeController.cs
@@ -1,21 +1,25 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FronEnd.Models;
+using FronEnd.Helpers.Interfaces;
 
 namespace FronEnd.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IPlatilloHelper _platilloHelper;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IPlatilloHelper platilloHelper)
     {
         _logger = logger;
+        _platilloHelper = platilloHelper;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var platillos = _platilloHelper.GetPlatillos();
+        return View(platillos);
     }
 
     public IActionResult Privacy()
